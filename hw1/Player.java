d8e95d42d2afe76726cee3fd713e7bf7253f39e2
Axel Samuelsson <limbero@n180-p222.kthopen.kth.se>
2015-09-08 13:54:06 +0200

    Made good code. Yes.

--- hw1/Player.java
+++ hw1/Player.java
@@ -3,6 +3,7 @@ import java.util.*;
 public class Player {
     static final int PLAYER_WHITE = 1,
                        PLAYER_RED = -1;
+    GameState bestState;
     /**
      * Performs a move
      *
@@ -27,8 +28,54 @@ public class Player {
          * the best next state. This skeleton returns a random move instead.
          */
 
-        return negaMax(pState, 10, );
+        negaMax(pState, 10, PLAYER_WHITE);
+        return bestState;
     }
 
+    public int negaMax(final GameState pState, int depth, int color) {
+        if (depth == 0 || pState.isEOG())
+            return color * evaluateBoard(pState);
 
+        Vector<GameState> lNextStates = new Vector<GameState>();
+        pState.findPossibleMoves(lNextStates);
+
+        int bestScore = Integer.MIN_VALUE;
+        bestState = lNextStates.get(0);
+
+        for (GameState lNextState : lNextStates) {
+             int val = -negaMax(lNextState, depth-1, -color);
+             bestScore = val > bestScore ? val : bestScore;
+             bestState = lNextState;
+        }
+        return bestScore;
+    }
+
+    public int evaluateBoard(pState) {
+
+        if (pState.isRedWin())
+            return Integer.MIN_VALUE;
+        else if (pState.isWhiteWin())
+            return Integer.MAX_VALUE;
+
+        int whitescore = 0, redscore = 0;
+        for (int i=0; i < 32; i++) {
+            int piece = pState.get(i);
+
+            if (0 != (piece & Constants.CELL_WHITE)) {
+                int tempscore = 1;
+                if (0 != (piece & Constants.CELL_KING)) {
+                    tempscore *= 5;
+                }
+                whitescore += tempscore;
+            } else if (0 != (piece & Constants.CELL_RED)) {
+                int tempscore = 1;
+                if (0 != (piece & Constants.CELL_KING)) {
+                    tempscore *= 5;
+                }
+                redscore += tempscore;
+            }
+        }
+
+        return whitescore - redscore;
+    }
 }
