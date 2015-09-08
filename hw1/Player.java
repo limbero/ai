101d6b39d703872a3ddb11cc33c5da90847edd03
Axel Samuelsson <limbero@n180-p222.kthopen.kth.se>
2015-09-08 13:17:04 +0200

    HW1 initial commit.

---
+++ hw1/Player.java
@@ -0,0 +1,34 @@
+import java.util.*;
+
+public class Player {
+    static final int PLAYER_WHITE = 1,
+                       PLAYER_RED = -1;
+    /**
+     * Performs a move
+     *
+     * @param pState
+     *            the current state of the board
+     * @param pDue
+     *            time before which we must have returned
+     * @return the next state the board is in after our move
+     */
+    public GameState play(final GameState pState, final Deadline pDue) {
+
+        Vector<GameState> lNextStates = new Vector<GameState>();
+        pState.findPossibleMoves(lNextStates);
+
+        if (lNextStates.size() == 0) {
+            // Must play "pass" move if there are no other moves possible.
+            return new GameState(pState, new Move());
+        }
+
+        /**
+         * Here you should write your algorithms to get the best next move, i.e.
+         * the best next state. This skeleton returns a random move instead.
+         */
+
+        return negaMax(pState, 10, );
+    }
+
+
+}
