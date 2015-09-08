c67239d573f83c4293a98cd886265ff927ba1940
Axel Samuelsson <limbero@n180-p222.kthopen.kth.se>
2015-09-08 15:01:32 +0200

    Alpha beta pruning.

--- hw1/Player.java
+++ hw1/Player.java
@@ -2,17 +2,17 @@ import java.util.*;
 
 public class Player {
     static final int PLAYER_WHITE = 1,
-                       PLAYER_RED = -1;
+    PLAYER_RED = -1;
     GameState bestState;
     /**
-     * Performs a move
-     *
-     * @param pState
-     *            the current state of the board
-     * @param pDue
-     *            time before which we must have returned
-     * @return the next state the board is in after our move
-     */
+    * Performs a move
+    *
+    * @param pState
+    *            the current state of the board
+    * @param pDue
+    *            time before which we must have returned
+    * @return the next state the board is in after our move
+    */
     public GameState play(final GameState pState, final Deadline pDue) {
 
         Vector<GameState> lNextStates = new Vector<GameState>();
@@ -24,17 +24,19 @@ public class Player {
         }
 
         /**
-         * Here you should write your algorithms to get the best next move, i.e.
-         * the best next state. This skeleton returns a random move instead.
-         */
+        * Here you should write your algorithms to get the best next move, i.e.
+        * the best next state. This skeleton returns a random move instead.
+        */
 
-        negaMax(pState, 10, PLAYER_WHITE);
+        int alpha = Integer.MIN_VALUE;
+        int beta = Integer.MAX_VALUE;
+        negaMax(pState, 15, PLAYER_WHITE, alpha, beta);
         return bestState;
     }
 
-    public int negaMax(final GameState pState, int depth, int color) {
+    public int negaMax(final GameState pState, int depth, int color, int alpha, int beta) {
         if (depth == 0 || pState.isEOG())
-            return color * evaluateBoard(pState);
+        return color * evaluateBoard(pState);
 
         Vector<GameState> lNextStates = new Vector<GameState>();
         pState.findPossibleMoves(lNextStates);
@@ -43,19 +45,23 @@ public class Player {
         bestState = lNextStates.get(0);
 
         for (GameState lNextState : lNextStates) {
-             int val = -negaMax(lNextState, depth-1, -color);
-             bestScore = val > bestScore ? val : bestScore;
-             bestState = lNextState;
+            int val = -negaMax(lNextState, depth-1, -color, -beta, -alpha);
+            bestScore = val > bestScore ? val : bestScore;
+            bestState = lNextState;
+
+            alpha = bestScore > alpha ? bestScore : alpha;
+            if (beta <= alpha)
+                break;
         }
         return bestScore;
     }
 
-    public int evaluateBoard(pState) {
+    public int evaluateBoard(final GameState pState) {
 
         if (pState.isRedWin())
-            return Integer.MIN_VALUE;
+        return Integer.MIN_VALUE;
         else if (pState.isWhiteWin())
-            return Integer.MAX_VALUE;
+        return Integer.MAX_VALUE;
 
         int whitescore = 0, redscore = 0;
         for (int i=0; i < 32; i++) {
